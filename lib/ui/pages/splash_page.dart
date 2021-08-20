a8b5e8d9aaf46e1ab0093c4bb745f01f218bea69
Ridha Ahmad Firdaus <41348944+RidhaAF@users.noreply.github.com>
2021-08-20 20:04:35 +0700

    Add Navigation & Routes

--- lib/ui/pages/splash_page.dart
+++ lib/ui/pages/splash_page.dart
@@ -1,8 +1,23 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:airplane_app/shared/theme.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  _SplashPageState createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    Timer(Duration(seconds: 3), () {
+      Navigator.pushNamed(context, '/get-started');
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
