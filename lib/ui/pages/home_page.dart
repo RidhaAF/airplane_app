ecb2d466d9cc00bb08368356aa6719f68c700be7
Ridha Ahmad Firdaus <41348944+RidhaAF@users.noreply.github.com>
2021-08-25 08:35:34 +0700

    Slicing Home - Popular Destinations

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -53,9 +53,79 @@ class HomePage extends StatelessWidget {
       );
     }
 
+    Widget popularDestionations() {
+      return Container(
+        margin: EdgeInsets.only(top: 30),
+        child: Row(
+          children: [
+            Container(
+              height: 320,
+              width: 200,
+              margin: EdgeInsets.only(left: defaultMargin),
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(defaultRadius),
+                color: kWhiteColor,
+              ),
+              child: Column(
+                children: [
+                  Container(
+                    height: 220,
+                    width: 180,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(defaultRadius),
+                      image: DecorationImage(
+                        image: AssetImage('assets/image_destination1.png'),
+                      ),
+                    ),
+                    child: Align(
+                      alignment: Alignment.topRight,
+                      child: Container(
+                        height: 30,
+                        width: 60,
+                        decoration: BoxDecoration(
+                          color: kWhiteColor,
+                          borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(defaultMargin),
+                          ),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              height: 20,
+                              width: 20,
+                              margin: EdgeInsets.only(right: 2),
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                  image: AssetImage('assets/icon_star.png'),
+                                ),
+                              ),
+                            ),
+                            Text(
+                              '4.8',
+                              style: blackTextStyle.copyWith(
+                                fontWeight: medium,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
     return ListView(
       children: [
         header(),
+        popularDestionations(),
       ],
     );
   }
