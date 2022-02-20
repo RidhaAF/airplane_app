2d0b57494934841c235c599e092c2f6d2afb2bc2
RidhaAF <ridhaaf@gmail.com>
2022-02-20 22:11:00 +0700

    Add Sign In Service & Cubit

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -29,6 +29,23 @@ class AuthService {
     }
   }
 
+  Future<UserModel> signIn(
+      {required String email, required String password}) async {
+    try {
+      UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      UserModel user =
+          await UserService().getUserById(userCredential.user!.uid);
+
+      return user;
+    } catch (e) {
+      throw e;
+    }
+  }
+
   Future<void> signOut() async {
     try {
       await _auth.signOut();
