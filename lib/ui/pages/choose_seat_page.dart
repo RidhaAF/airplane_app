1372fdb47660cc1dcfefe403a9e7cecfaae3c76c
RidhaAF <ridhaaf@gmail.com>
2022-02-20 17:47:46 +0700

    Choose Seat Page - Price & Button

--- lib/ui/pages/choose_seat_page.dart
+++ lib/ui/pages/choose_seat_page.dart
@@ -1,4 +1,5 @@
 import 'package:airplane_app/shared/theme.dart';
+import 'package:airplane_app/ui/widgets/custom_button.dart';
 import 'package:airplane_app/ui/widgets/seat_item.dart';
 import 'package:flutter/material.dart';
 
@@ -333,7 +334,64 @@ class ChooseSeatPage extends StatelessWidget {
                 ],
               ),
             ),
+
+            // NOTE: YOUR SEAT
+            Container(
+              margin: EdgeInsets.only(top: 30),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Your seat',
+                    style: greyTextStyle.copyWith(
+                      fontWeight: light,
+                    ),
+                  ),
+                  Text(
+                    'A3, B3',
+                    style: blackTextStyle.copyWith(
+                      fontSize: 16,
+                      fontWeight: medium,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            // NOTE: TOTAL
+            Container(
+              margin: EdgeInsets.only(top: 16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Total',
+                    style: greyTextStyle.copyWith(
+                      fontWeight: light,
+                    ),
+                  ),
+                  Text(
+                    'IDR 540.000.000',
+                    style: purpleTextStyle.copyWith(
+                      fontSize: 16,
+                      fontWeight: semiBold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
+        ),
+      );
+    }
+
+    Widget checkoutButton() {
+      return CustomButton(
+        title: 'Continue to Checkout',
+        onPressed: () {},
+        margin: EdgeInsets.only(
+          top: 30,
+          bottom: defaultMargin * 2,
         ),
       );
     }
@@ -346,6 +404,7 @@ class ChooseSeatPage extends StatelessWidget {
           title(),
           seatStatus(),
           selectSeeat(),
+          checkoutButton(),
         ],
       ),
     );
