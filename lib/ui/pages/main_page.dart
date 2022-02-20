9d349d61c731838793f5fb3d5d5d55643a3c1854
RidhaAF <ridhaaf@gmail.com>
2022-02-20 19:20:35 +0700

    Add Cubit Page at Home Page

--- lib/ui/pages/main_page.dart
+++ lib/ui/pages/main_page.dart
@@ -1,15 +1,31 @@
+import 'package:airplane_app/cubit/page_cubit.dart';
 import 'package:airplane_app/shared/theme.dart';
 import 'package:airplane_app/ui/pages/home_page.dart';
+import 'package:airplane_app/ui/pages/setting_page.dart';
+import 'package:airplane_app/ui/pages/transaction_page.dart';
+import 'package:airplane_app/ui/pages/wallet_page.dart';
 import 'package:airplane_app/ui/widgets/custom_bottom_navigation_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    Widget buildContent() {
-      return HomePage();
+    Widget buildContent(int currentIndex) {
+      switch (currentIndex) {
+        case 0:
+          return HomePage();
+        case 1:
+          return TransactionPage();
+        case 2:
+          return WalletPage();
+        case 3:
+          return SettingPage();
+        default:
+          return HomePage();
+      }
     }
 
     Widget customBottomNavigation() {
@@ -30,17 +46,20 @@ class MainPage extends StatelessWidget {
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              CustomBottomNaviagtionItem(
+              CustomBottomNavigationItem(
+                index: 0,
                 imageUrl: 'assets/icon_home.png',
-                isSelected: true,
               ),
-              CustomBottomNaviagtionItem(
+              CustomBottomNavigationItem(
+                index: 1,
                 imageUrl: 'assets/icon_booking.png',
               ),
-              CustomBottomNaviagtionItem(
+              CustomBottomNavigationItem(
+                index: 2,
                 imageUrl: 'assets/icon_card.png',
               ),
-              CustomBottomNaviagtionItem(
+              CustomBottomNavigationItem(
+                index: 3,
                 imageUrl: 'assets/icon_settings.png',
               ),
             ],
@@ -49,14 +68,18 @@ class MainPage extends StatelessWidget {
       );
     }
 
-    return Scaffold(
-      backgroundColor: kBackgroundColor,
-      body: Stack(
-        children: [
-          buildContent(),
-          customBottomNavigation(),
-        ],
-      ),
+    return BlocBuilder<PageCubit, int>(
+      builder: (context, currentIndex) {
+        return Scaffold(
+          backgroundColor: kBackgroundColor,
+          body: Stack(
+            children: [
+              buildContent(currentIndex),
+              customBottomNavigation(),
+            ],
+          ),
+        );
+      },
     );
   }
 }
--- lib/ui/widgets/custom_bottom_navigation_item.dart
+++ lib/ui/widgets/custom_bottom_navigation_item.dart
@@ -1,40 +1,48 @@
+import 'package:airplane_app/cubit/page_cubit.dart';
 import 'package:airplane_app/shared/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class CustomBottomNaviagtionItem extends StatelessWidget {
+class CustomBottomNavigationItem extends StatelessWidget {
+  final int index;
   final String imageUrl;
-  final bool isSelected;
 
-  const CustomBottomNaviagtionItem({
+  const CustomBottomNavigationItem({
     Key? key,
+    required this.index,
     required this.imageUrl,
-    this.isSelected = false,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        SizedBox(),
-        Container(
-          height: 24,
-          width: 24,
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage(imageUrl),
+    return GestureDetector(
+      onTap: () {
+        context.read<PageCubit>().setPage(index);
+      },
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          SizedBox(),
+          Image.asset(
+            imageUrl,
+            width: 24,
+            height: 24,
+            color: context.read<PageCubit>().state == index
+                ? kPrimaryColor
+                : kGreyColor,
+          ),
+          Container(
+            height: 2,
+            width: 30,
+            decoration: BoxDecoration(
+              color: context.read<PageCubit>().state == index
+                  ? kPrimaryColor
+                  : kTransparentColor,
+              borderRadius: BorderRadius.circular(defaultRadius),
             ),
           ),
-        ),
-        Container(
-          height: 2,
-          width: 30,
-          decoration: BoxDecoration(
-            color: isSelected ? kPrimaryColor : kTransparentColor,
-            borderRadius: BorderRadius.circular(defaultRadius),
-          ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
